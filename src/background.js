991222808da5298aec3986fe06ae5bd375d8298d
eight04 <eight04@gmail.com>
2024-08-30 22:43:38 +0800

    Fix: webRequest doesn't work on Firefox

--- src/background.js
+++ src/background.js
@@ -8,24 +8,26 @@ const filters = [
 ];
 
 for (const f of filters) {
+  // NOTE: on Firefox, you can't register the same listener multiple times
+  // so we need to use a different function for each filter
+  const overrideHeaders = ({requestHeaders}) => {
+    for (const header of requestHeaders) {
+      switch (header.name.toLowerCase()) {
+        case "user-agent":
+          header.value = modifyUA(header.value);
+          break;
+        case "sec-ch-ua-mobile":
+          header.value = "?0";
+          break;
+      }
+    }
+    return {requestHeaders};
+  };
   browser.webRequest.onBeforeSendHeaders.addListener(
     overrideHeaders, f, ["blocking", "requestHeaders"]
   );
 }
 
-function overrideHeaders({requestHeaders}) {
-  for (const header of requestHeaders) {
-    switch (header.name.toLowerCase()) {
-      case "user-agent":
-        header.value = modifyUA(header.value);
-        break;
-      case "sec-ch-ua-mobile":
-        header.value = "?0";
-        break;
-    }
-  }
-  return {requestHeaders};
-}
 
 browser.webNavigation.onHistoryStateUpdated.addListener(({tabId}) => {
   browser.tabs.sendMessage(tabId, {method: "historyStateUpdated"});
